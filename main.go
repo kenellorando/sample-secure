5c0342d7a1005fa4c0b375491c52ef8f147444e0
kenellorando <kenellorando@gmail.com>
2019-04-07 16:46:19 -0500

    Initial commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    "crypto/tls"
+    "log"
+    "net/http"
+
+    "golang.org/x/crypto/acme/autocert"
+)
+
+func main() {
+    certManager := autocert.Manager{
+        Prompt:     autocert.AcceptTOS,
+        HostPolicy: autocert.HostWhitelist("example.com"), //Your domain here
+        Cache:      autocert.DirCache("certs"),                   //Folder for storing certificates
+    }
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("Hello world"))
+    })
+
+    server := &http.Server{
+        Addr: ":https",
+        TLSConfig: &tls.Config{
+            GetCertificate: certManager.GetCertificate,
+        },
+    }
+
+    go http.ListenAndServe(":http", certManager.HTTPHandler(nil))
+
+    log.Fatal(server.ListenAndServeTLS("", "")) //Key and cert are coming from Let's Encrypt
+}
